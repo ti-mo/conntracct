406ff7db8b2ce0eb16887e3009065ada29e05eac
Timo Beckers <timo@incline.eu>
2018-07-11 16:46:03 +0200

    internal/pprof - package for registering pprof on localhost:6060

---
+++ internal/pprof/pprof.go
@@ -0,0 +1,25 @@
+package pprof
+
+import (
+	"log"
+	"net/http"
+
+	// side effect of registering HTTP handler in default ServeMux
+	_ "net/http/pprof"
+)
+
+// ListenAndServe starts a pprof endpoint on localhost:6060
+// and replaces the global http.DefaultServeMux with a new instance.
+func ListenAndServe() {
+
+	// Save a reference to the default global ServeMux
+	ppm := http.DefaultServeMux
+
+	// Replace the default ServeMux with a new instance
+	http.DefaultServeMux = http.NewServeMux()
+
+	// Start pprof server on global ServeMux
+	go func() {
+		log.Fatal(http.ListenAndServe("localhost:6060", ppm))
+	}()
+}
