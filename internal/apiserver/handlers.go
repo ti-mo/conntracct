958814b71bb2d307f74e8dc6b43a41bb6c673768
Timo Beckers <timo@incline.eu>
2018-07-15 14:57:06 +0200

    internal/apiserver - dump all sink statistics in /stats

--- internal/apiserver/handlers.go
+++ internal/apiserver/handlers.go
@@ -11,4 +11,8 @@ func HandleStats(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	fmt.Fprintf(w, "Pipeline: %v\n", pipe.Stats)
+
+	for _, s := range pipe.GetAcctSinks() {
+		fmt.Fprintf(w, "Sink '%s': %v\n", s.Name(), s.Stats())
+	}
 }
