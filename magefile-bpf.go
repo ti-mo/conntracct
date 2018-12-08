e3caa6a32657cb4aa4624065404a73a935c118f5
Timo Beckers <timo@incline.eu>
2018-12-08 16:41:41 +0100

    magefile for binary and BPF objects, update .gitignore

---
+++ magefile-bpf.go
@@ -0,0 +1,183 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+
+	"github.com/magefile/mage/target"
+
+	"github.com/magefile/mage/mg"
+	"gitlab.com/0ptr/conntracct/pkg/kernel"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	bpfBuildPath = "build/bpf/"
+	bpfAcctProbe = "bpf/acct.c"
+)
+
+// Bpf is the namespace for all BPF-related build tasks.
+type Bpf mg.Namespace
+
+func (Bpf) Clean() error {
+
+	fmt.Println("Removing directory", bpfBuildPath, "..")
+	if err := os.RemoveAll(bpfBuildPath); err != nil {
+		return err
+	}
+
+	for _, k := range kernel.Builds {
+		fmt.Println("Removing directory", k.Directory(), "..")
+		if err := os.RemoveAll(k.Directory()); err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("So fresh, so clean.")
+	return nil
+}
+
+// Build builds all BPF programs against all defined kernels.
+func (Bpf) Build() error {
+
+	// Create build target directory.
+	if err := os.MkdirAll(bpfBuildPath, os.ModePerm); err != nil {
+		return err
+	}
+
+	// Build the acct probe against all Kernels defined in the kernel package.
+	for _, k := range kernel.Builds {
+
+		// Name of the resulting BPF object file.
+		bpfObjectName := fmt.Sprintf("acct-%s.o", k.Version)
+
+		// Target path for the compiled BPF object.
+		bpfObjectPath := path.Join(bpfBuildPath, bpfObjectName)
+
+		// Check if the acct probe source is newer than the probe's object in the build directory.
+		run, err := target.Path(bpfObjectPath, bpfAcctProbe)
+		if err != nil {
+			return err
+		}
+
+		// Skip this build if the object is newer than the source.
+		if !run {
+			fmt.Println("Acct probe is up-to-date:", bpfObjectPath)
+			continue
+		}
+
+		// Download and extract all kernels first.
+		mg.Deps(Bpf.Kernels)
+
+		if err := buildProbe(bpfAcctProbe, bpfObjectPath, k.Directory()); err != nil {
+			fmt.Println("Failed to build probe against kernel", k.Version)
+			return err
+		}
+
+		fmt.Println("Built acct probe", bpfObjectName)
+	}
+
+	return nil
+}
+
+// Kernels downloads and extracts a list of kernels to a temporary directory.
+func (Bpf) Kernels() error {
+
+	var eg errgroup.Group
+
+	for _, k := range kernel.Builds {
+
+		// https://golang.org/doc/faq#closures_and_goroutines
+		k := k
+
+		eg.Go(func() error {
+			// Get and unarchive the kernel.
+			if err := k.Fetch(); err != nil {
+				return err
+			}
+
+			// Configure the kernel with its specified parameters.
+			if err := k.Configure(nil); err != nil {
+				return err
+			}
+
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// buildProbe builds a BPF program given its source file, destination object file
+// and directory of the kernel source tree the program is to be built against.
+func buildProbe(srcFile, destObj, kernelDir string) error {
+
+	clangParams := []string{
+		"-D__KERNEL__", "-D__BPF_TRACING__",
+		"-fno-stack-protector",
+		"-Wno-address-of-packed-member",
+		"-Wno-gnu-variable-sized-type-not-at-end",
+		"-Wno-unused-value",
+		"-Wno-pointer-sign",
+		"-Wno-compare-distinct-pointer-types",
+		"-Wunused", "-Wall", "-Werror",
+		"-O2", "-emit-llvm", "-ferror-limit=1",
+		"-c", srcFile, // Input file
+		"-o", "-", // Output to stdout
+	}
+
+	kdirs := []string{
+		"-I%s/include",
+		"-I%s/include/uapi",
+		"-I%s/arch/x86/include",
+		"-I%s/arch/x86/include/generated",
+		// "-I%s/arch/x86/include/uapi",
+		// "-I%s/include/generated/uapi",
+	}
+
+	// Resolve kernel directories in all include paths and append to clang params.
+	for _, d := range kdirs {
+		clangParams = append(clangParams, fmt.Sprintf(d, kernelDir))
+	}
+
+	llcParams := []string{
+		"-march=bpf",
+		"-filetype=obj",
+		"-o", destObj,
+	}
+
+	clang := exec.Command("clang", clangParams...)
+	llc := exec.Command("llc", llcParams...)
+
+	// Redirect stderr of the builds to the terminal's stderr.
+	clang.Stderr = os.Stderr
+	llc.Stderr = os.Stderr
+
+	llc.Stdin, _ = clang.StdoutPipe()
+	llc.Stdout = os.Stdout
+
+	if err := llc.Start(); err != nil {
+		return err
+	}
+
+	// Run clang and wait for it to finish.
+	if err := clang.Run(); err != nil {
+		fmt.Println("Error running clang with args:", clang.Args)
+		return err
+	}
+
+	if err := llc.Wait(); err != nil {
+		fmt.Println("Error running llc with args:", llc.Args)
+		return err
+	}
+
+	return nil
+}
---
+++ magefile.go
@@ -0,0 +1,96 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/magefile/mage/sh"
+	"github.com/magefile/mage/target"
+)
+
+const (
+	app = "conntracct"
+)
+
+// Build builds the application.
+func Build() error {
+
+	// Watch for files newer than the app in these directories.
+	mod, err := target.Dir(app, "bpf", "pkg", "internal")
+	if err != nil {
+		return err
+	}
+
+	if mod {
+
+		realPath := realPath(app)
+
+		// Unlink the existing binary so it can be replaced without stopping the daemon first.
+		if err := sh.Rm(realPath); err != nil {
+			return err
+		}
+
+		// Build the application
+		if err := sh.Run("go", "build"); err != nil {
+			return err
+		}
+
+		// 'Minimal' capability set to run without being uid 0.
+		// cap_sys_admin for calling bpf().
+		// cap_ipc_lock for locking memory for the ring buffer.
+		// cap_dac_override for opening /sys/kernel/debug/tracing/*
+		// cap_net_admin for managing sysctl net.netfilter.nf_conntrack_acct
+		if err := sh.Run("sudo", "setcap", "cap_sys_admin,cap_ipc_lock,cap_net_admin,cap_dac_override+eip", realPath); err != nil {
+			return err
+		}
+
+		fmt.Printf("Successfully built %s!\n", app)
+		return nil
+	}
+
+	fmt.Println(app, "already up to date.")
+	return nil
+}
+
+// Dev brings up a docker-compose stack and runs the application with modd for live reloading.
+func Dev() error {
+
+	if err := sh.Run("docker-compose", "-f", "test/docker-compose.yml", "-p", app, "up", "-d"); err != nil {
+		return err
+	}
+
+	fmt.Println("Starting live-reload with modd..")
+	if err := sh.RunV("modd"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// realPath resolves (nested) symlinks. If the target of a nested symlink does
+// not exist, falls back to the target of the first symlink.
+func realPath(path string) string {
+
+	fi, err := os.Lstat(path)
+	if err != nil {
+		// Return the input string if the path doesn't exist (yet), there's nothing to resolve.
+		return path
+	}
+
+	if (fi.Mode() & os.ModeSymlink) != 0 {
+		// Try to recursive symlinks.
+		realPath, err := filepath.EvalSymlinks(path)
+		if err != nil {
+			// Return the symlink target path if target file doesn't exist.
+			ret, _ := os.Readlink(path)
+			return ret
+		}
+
+		return realPath
+	}
+
+	return path
+}
