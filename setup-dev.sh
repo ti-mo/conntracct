9b5091af9762f48a4938a4ded7c47b9dc6828c16
Timo Beckers <timo@incline.eu>
2018-12-11 14:59:57 +0100

    setup-dev - remove acct sysctl

--- setup-dev.sh
+++ setup-dev.sh
@@ -2,7 +2,5 @@ set -x
 
 # conntrack module is not loaded (and thus eBPF probe cannot be loaded)
 # so iptables needs to have a conntrack rule if we don't want to call modprobe manually
-# check /proc/kallsyms if a probe fails to attach, the symbol might be missing
 sudo ip6tables -t filter -A OUTPUT -m conntrack --ctstate related,established -j ACCEPT
 sudo iptables -t filter -A OUTPUT -m conntrack --ctstate related,established -j ACCEPT
-sudo sysctl net.netfilter.nf_conntrack_acct=1
