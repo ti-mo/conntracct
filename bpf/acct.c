65606a14abaf034cc7110e3a0c62fd8eeb0f71cf
Timo Beckers <timo@incline.eu>
2019-01-14 21:12:35 +0100

    bpf/acct.c - extract acct_ext, counter, tuple and netns logic into funcs

--- bpf/acct.c
+++ bpf/acct.c
@@ -21,6 +21,83 @@ struct acct_event_t {
   u8 proto;
 };
 
+// get_acct_ext gets a reference to the nf_conn's accounting extension.
+// Returns non-zero on error.
+__attribute__((always_inline))
+static int get_acct_ext(struct nf_conn_acct **acct_ext, struct nf_conn *ct) {
+
+  // Check if accounting extension is enabled and initialized
+  // for this connection. Important because the acct codepath
+  // is called for unix socket usage as well. Also, the acct
+  // extension memory is uninitialized if the acct sysctl is disabled.
+  struct nf_ct_ext *ct_ext;
+  bpf_probe_read(&ct_ext, sizeof(ct_ext), &ct->ext);
+  if (!ct_ext)
+    return -1;
+
+  u8 ct_acct_offset;
+  bpf_probe_read(&ct_acct_offset, sizeof(ct_acct_offset), &ct_ext->offset[NF_CT_EXT_ACCT]);
+  if (!ct_acct_offset)
+    return -1;
+
+  // Obtain reference to accounting conntrack extension.
+  *acct_ext = ((void *)ct_ext + ct_acct_offset);
+  if (!*acct_ext)
+    return -1;
+
+  return 0;
+}
+
+// extract_counters extracts accounting info from an nf_conn_act into acct_event_t.
+__attribute__((always_inline))
+static void extract_counters(struct acct_event_t *data, struct nf_conn_acct *acct_ext) {
+
+  struct nf_conn_counter ctr[IP_CT_DIR_MAX];
+  bpf_probe_read(&ctr, sizeof(ctr), &acct_ext->counter);
+
+  data->packets_orig = ctr[IP_CT_DIR_ORIGINAL].packets.counter;
+  data->bytes_orig = ctr[IP_CT_DIR_ORIGINAL].bytes.counter;
+
+  data->packets_ret = ctr[IP_CT_DIR_REPLY].packets.counter;
+  data->bytes_ret = ctr[IP_CT_DIR_REPLY].bytes.counter;
+
+}
+
+// extract_tuple extracts tuple information (proto, src/dest ip and port) of an nf_conn
+// into an acct_event_t.
+__attribute__((always_inline))
+static void extract_tuple(struct acct_event_t *data, struct nf_conn *ct) {
+
+  struct nf_conntrack_tuple_hash tuplehash[IP_CT_DIR_MAX];
+  bpf_probe_read(&tuplehash, sizeof(tuplehash), &ct->tuplehash);
+
+  data->proto = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.protonum;
+
+  data->srcaddr = tuplehash[IP_CT_DIR_ORIGINAL].tuple.src.u3;
+  data->dstaddr = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.u3;
+
+  data->srcport = tuplehash[IP_CT_DIR_ORIGINAL].tuple.src.u.all;
+  data->dstport = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.u.all;
+
+}
+
+// extract_netns extracts the nf_conn's network namespace inode number into an acct_event_t.
+__attribute__((always_inline))
+static void extract_netns(struct acct_event_t *data, struct nf_conn *ct) {
+
+  // Obtain reference to network namespace.
+  // Warning: ct_net is a possible_net_t with a single member,
+  // so we read `struct net` instead at the same location. Reading
+  // the `*net` in `possible_net_t` will yield a (non-zero) garbage value.
+  struct net *net;
+  bpf_probe_read(&net, sizeof(net), &ct->ct_net);
+
+  if (net) {
+    // netns field will remain zero if probe read fails.
+    bpf_probe_read(&data->netns, sizeof(data->netns), &net->ns.inum);
+  }
+}
+
 struct bpf_map_def SEC("maps/acct_events") acct_events = {
 	.type = BPF_MAP_TYPE_PERF_EVENT_ARRAY,
 	.key_size = sizeof(int),
@@ -83,27 +160,13 @@ int kretprobe____nf_ct_refresh_acct(struct pt_regs *ctx) {
 	if (ctp == 0)
 		return 0;
 
-  // Dereference and delete from the stash table
+  // Dereference and delete from the stash table.
   struct nf_conn *ct = *ctp;
   bpf_map_delete_elem(&currct, &pid);
 
-  // Check if accounting extension is enabled and initialized
-  // for this connection. Important because the acct codepath
-  // is called for unix socket usage as well. Also, the acct
-  // extension memory is uninitialized if the acct sysctl is disabled.
-  struct nf_ct_ext *ct_ext;
-  bpf_probe_read(&ct_ext, sizeof(ct_ext), &ct->ext);
-  if (!ct_ext)
-    return 0;
-
-  u8 ct_acct_offset;
-  bpf_probe_read(&ct_acct_offset, sizeof(ct_acct_offset), &ct_ext->offset[NF_CT_EXT_ACCT]);
-  if (!ct_acct_offset)
-    return 0;
-
-  // Obtain reference to accounting conntrack extension
-  struct nf_conn_acct *acct_ext = ((void *)ct_ext + ct_acct_offset);
-  if (!acct_ext)
+  // Obtain reference to accounting conntrack extension.
+  struct nf_conn_acct *acct_ext = 0;
+  if (get_acct_ext(&acct_ext, ct))
     return 0;
 
   // Initialize cooldown value in the config map to 2 seconds.
@@ -111,7 +174,7 @@ int kretprobe____nf_ct_refresh_acct(struct pt_regs *ctx) {
   u64 def_cd = 2000000000;
   bpf_map_update_elem(&config, &config_cd, &def_cd, BPF_NOEXIST);
 
-  // Allocate event struct after all checks have succeeded
+  // Allocate event struct after all checks have succeeded.
   struct acct_event_t data = {
     .cid = (u32)ct,
     .ts = ts,
@@ -119,14 +182,7 @@ int kretprobe____nf_ct_refresh_acct(struct pt_regs *ctx) {
 
   // Pull counters onto the BPF stack first, so that we can make event rate
   // limiting decisions based on packet counters without doing unnecessary work.
-  struct nf_conn_counter ctr[IP_CT_DIR_MAX];
-  bpf_probe_read(&ctr, sizeof(ctr), &acct_ext->counter);
-
-  data.packets_orig = ctr[IP_CT_DIR_ORIGINAL].packets.counter;
-  data.bytes_orig = ctr[IP_CT_DIR_ORIGINAL].bytes.counter;
-
-  data.packets_ret = ctr[IP_CT_DIR_REPLY].packets.counter;
-  data.bytes_ret = ctr[IP_CT_DIR_REPLY].bytes.counter;
+  extract_counters(&data, acct_ext);
 
   // Sample accounting events from the kernel using a hybrid rate limiting model.
   // On every event that is sent, a future deadline is set for that specific flow
@@ -167,30 +223,12 @@ int kretprobe____nf_ct_refresh_acct(struct pt_regs *ctx) {
     }
   }
 
-  struct nf_conntrack_tuple_hash tuplehash[IP_CT_DIR_MAX];
-  bpf_probe_read(&tuplehash, sizeof(tuplehash), &ct->tuplehash);
-
-  data.proto = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.protonum;
-
-  data.srcaddr = tuplehash[IP_CT_DIR_ORIGINAL].tuple.src.u3;
-  data.dstaddr = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.u3;
-
-  data.srcport = tuplehash[IP_CT_DIR_ORIGINAL].tuple.src.u.all;
-  data.dstport = tuplehash[IP_CT_DIR_ORIGINAL].tuple.dst.u.all;
-
+  // Extract proto, src/dst address and ports.
+  extract_tuple(&data, ct);
+  // Extract network namespace identifier (inode).
+  extract_netns(&data, ct);
+  // Extract conntrack connection mark.
   bpf_probe_read(&data.connmark, sizeof(data.connmark), &ct->mark);
-
-  // Obtain reference to network namespace.
-  // Warning: ct_net is a possible_net_t with a single member,
-  // so we read `struct net` instead at the same location. Reading
-  // the `*net` in `possible_net_t` will yield a (non-zero) garbage value.
-  struct net *net;
-  bpf_probe_read(&net, sizeof(net), &ct->ct_net);
-
-  if (net) {
-    // netns field will remain zero if probe read fails.
-    bpf_probe_read(&data.netns, sizeof(data.netns), &net->ns.inum);
-  }
 
   // Submit event to userspace.
   bpf_perf_event_output(ctx, &acct_events, CUR_CPU_IDENTIFIER, &data, sizeof(data));
@@ -207,7 +245,7 @@ int kprobe__nf_conntrack_free(struct pt_regs *ctx) {
 
   struct nf_conn *ct = (struct nf_conn *) PT_REGS_PARM1(ctx);
 
-  // Remove next-update entry for connection
+  // Remove next-update entry for connection.
   bpf_map_delete_elem(&nextupd, &ct);
 
   return 0;
@@ -215,6 +253,4 @@ int kprobe__nf_conntrack_free(struct pt_regs *ctx) {
 
 char _license[] SEC("license") = "GPL";
 
-// this number will be interpreted by gobpf-elf-loader to set the current
-// running kernel version
 __u32 _version SEC("version") = 0xFFFFFFFE;
