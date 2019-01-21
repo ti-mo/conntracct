737eb59d028ee1333c687776cab323a6e8cc322e
Timo Beckers <timo@incline.eu>
2019-01-21 19:47:13 +0100

    setup-dev.sh - add sysctls for conntrack timeouts

--- setup-dev.sh
+++ setup-dev.sh
@@ -4,3 +4,9 @@ set -x
 # so iptables needs to have a conntrack rule if we don't want to call modprobe manually
 sudo ip6tables -t filter -A OUTPUT -m conntrack --ctstate related,established -j ACCEPT
 sudo iptables -t filter -A OUTPUT -m conntrack --ctstate related,established -j ACCEPT
+
+sudo sysctl net/netfilter/nf_conntrack_tcp_timeout_close_wait=15
+sudo sysctl net/netfilter/nf_conntrack_tcp_timeout_fin_wait=15
+sudo sysctl net/netfilter/nf_conntrack_tcp_timeout_time_wait=15
+sudo sysctl net/netfilter/nf_conntrack_udp_timeout=10
+sudo sysctl net/netfilter/nf_conntrack_udp_timeout_stream=30
