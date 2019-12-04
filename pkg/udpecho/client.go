e2973c486ce51b836c0c37f69b594d94326ba3ea
Timo Beckers <timo@incline.eu>
2019-12-04 01:00:58 +0100

    udpecho - add synchronization around MockUDP{Client,Server}.Close()

--- pkg/udpecho/client.go
+++ pkg/udpecho/client.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"time"
 )
 
 var (
@@ -12,16 +14,17 @@ var (
 	nop  = []byte("nop")  // no-op packet to send when not expecting a response
 )
 
-// MockUDP holds a UDP socket to localhost
-type MockUDP struct {
+// MockUDPClient holds a local UDP socket.
+type MockUDPClient struct {
+	mu   sync.RWMutex
 	conn *net.UDPConn
 	ctrl chan bool
 }
 
 // Dial opens a UDP socket to the given port on localhost.
-// Returns a new MockUDP.
+// Returns a new MockUDPClient.
 // When host is an empty string, connects to 127.0.1.1 by default.
-func Dial(host string, port uint16) MockUDP {
+func Dial(host string, port uint16) *MockUDPClient {
 
 	if host == "" {
 		host = "127.0.1.1"
@@ -39,44 +42,100 @@ func Dial(host string, port uint16) MockUDP {
 		log.Fatalln("error opening client listener:", err)
 	}
 
-	ctrl := make(chan bool)
-
-	go clientWorker(ctrl, conn)
-
-	return MockUDP{
+	m := MockUDPClient{
 		conn: conn,
-		ctrl: ctrl,
+		ctrl: make(chan bool),
 	}
+
+	go m.worker()
+
+	return &m
 }
 
-// Close closes the MockUDPs control channel and connection,
+// Close closes the MockUDPClients control channel and connection,
 // in that order.
-func (m MockUDP) Close() {
+func (m *MockUDPClient) Close() {
+
+	// Wait for a write lock to avoid closing the socket during an r/w operation.
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	close(m.ctrl)
 	_ = m.conn.Close()
 }
 
 // Ping generates num outgoing and expected return packets
 // on the connection to localhost.
-func (m MockUDP) Ping(num uint) {
+func (m *MockUDPClient) Ping(num uint) {
 	for i := 0; i < int(num); i++ {
 		m.ctrl <- true
 	}
 }
 
 // Nop generates num outgoing packets on the connection to localhost.
-func (m MockUDP) Nop(num uint) {
+func (m *MockUDPClient) Nop(num uint) {
 	for i := 0; i < int(num); i++ {
 		m.ctrl <- false
 	}
 }
 
-// ClientAddr returns the client address of the MockUDP socket.
-func (m MockUDP) ClientAddr() *net.UDPAddr {
+// ClientAddr returns the client address of the MockUDPClient socket.
+func (m *MockUDPClient) ClientAddr() *net.UDPAddr {
 	return m.conn.LocalAddr().(*net.UDPAddr)
 }
 
 // ClientPort returns the auto-generated client port of the connection.
-func (m MockUDP) ClientPort() uint16 {
+func (m *MockUDPClient) ClientPort() uint16 {
 	return uint16(m.ClientAddr().Port)
+}
+
+func (m *MockUDPClient) worker() {
+
+	b := make([]byte, 65507)
+
+	for {
+		// Wait for a control message.
+		c, ok := <-m.ctrl
+		if !ok {
+			// Channel closed, stop goroutine.
+			return
+		}
+
+		// Obtain lock to the client.
+		m.mu.RLock()
+
+		if c {
+
+			// Write ping message to connection.
+			n, err := m.conn.Write(ping)
+			if err != nil || n != len(ping) {
+				log.Fatalln("error writing ping to conn:", err)
+			}
+
+			// Set the read deadline to 20ms in the future.
+			if err = m.conn.SetReadDeadline(time.Now().Add(20 * time.Millisecond)); err != nil {
+				log.Fatalln("error setting read deadline:", err)
+			}
+
+			// Expect a response from the server in case payload is 'ping'.
+			n, _, err = m.conn.ReadFromUDP(b)
+			if err != nil {
+				log.Fatalln("error reading packet from conn:", err)
+			}
+
+			// Expect the response to be 'pong', halt when that's not the case.
+			if string(b[:n]) != string(pong) {
+				log.Fatalf("expected UDP response '%s', got '%s'", pong, b[:n])
+			}
+		} else {
+			// Write nop message to connection, don't expect a response.
+			n, err := m.conn.Write(nop)
+			if err != nil || n != len(nop) {
+				log.Fatalln("error writing nop to conn:", err)
+			}
+		}
+
+		// Release client's read lock.
+		m.mu.RUnlock()
+	}
 }
--- pkg/udpecho/server.go
+++ pkg/udpecho/server.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
+
+// MockUDPServer holds a mock UDP server.
+type MockUDPServer struct {
+	mu   sync.RWMutex
+	conn net.PacketConn
+}
 
 // ListenAndEcho starts a UDP listener that replies 'pong'
 // in response to a request packet containing 'ping'.
 // When host is an empty string, listens on 127.0.1.1 by default.
-func ListenAndEcho(host string, port uint16) net.PacketConn {
+func ListenAndEcho(host string, port uint16) *MockUDPServer {
 
 	if host == "" {
 		host = "127.0.1.1"
@@ -20,7 +27,45 @@ func ListenAndEcho(host string, port uint16) net.PacketConn {
 		log.Fatalln("error opening UDP listener:", err)
 	}
 
-	go serverWorker(l)
+	s := MockUDPServer{
+		conn: l,
+	}
 
-	return l
+	go s.worker()
+
+	return &s
+}
+
+func (s *MockUDPServer) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	return s.conn.Close()
+}
+
+func (s *MockUDPServer) worker() {
+
+	b := make([]byte, 65507)
+
+	for {
+		n, addr, err := s.conn.ReadFrom(b)
+		if err != nil {
+			// Stop goroutine if socket is closed.
+			return
+		}
+
+		// Reply 'pong' to the sender when payload is 'ping'.
+		if string(b[:n]) == string(ping) {
+
+			s.mu.RLock()
+
+			_, err = s.conn.WriteTo(pong, addr)
+			if err != nil {
+				log.Fatalln("error echoing message to socket:", err)
+			}
+
+			s.mu.RUnlock()
+		}
+	}
+
 }
